0c034621a38e1e65300ada72ad28d4779cdde0a7
AdamMajster <adam.dunowski@o2.pl>
2024-04-05 09:47:11 +0200

    dni tygodnia

---
+++ main.py
@@ -0,0 +1,15 @@
+CREATE TABLE dni_tygodnia (
+    id_dnia INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id_dnia)
+);
+
+
+INSERT INTO dni_tygodnia (id_dnia) VALUES
+('Poniedziałek'),
+('Wtorek'),
+('Środa'),
+('Czwartek'),
+('Piątek'),
+('Sobota'),
+('Niedziela')
+;
